8be8af2ae6fa1acbcd73ad8b7446de0b86d2a241
Olle Ronstad <Olle.Ronstad@gmail.com>
2025-02-06 19:25:33 +0100

    added qury struct wich can call next on, which then reruns new query structs which have traversed allong once

--- src/pathExpression/query.go
+++ src/pathExpression/query.go
@@ -19,21 +19,21 @@ type DataNode struct {
 	edges []DataEdge
 }
 type DataEdge struct {
-	name      string
-	target_id int
+	EdgeName   string
+	TargetName string
 }
 
 // struct to represent all the info we need in the query
 type QueryStruct struct {
 	Query       string    // the query as a string
 	Rootpointer *RootNode // pointer to the tree
-	CurrentLeaf *LeafNode // the path that should be taken to the next node
+	FollowLeaf  *LeafNode // the path that should be taken to the next node
 	NextNode    string    // the name of the node next node
 }
 
 // creates and returns QueryStruct from a query string
 // if the string matches waht querystruct.Tostring
-func bobTheBuilder(input_query string, data map[int]DataNode) (QueryStruct, error) {
+func bobTheBuilder(input_query string, data map[string][]DataEdge) (QueryStruct, error) {
 	// pre process query, remove spaces and change
 	input_query = preprocessQuery(input_query)
 
@@ -49,38 +49,81 @@ func bobTheBuilder(input_query string, data map[int]DataNode) (QueryStruct, erro
 
 	// TODO this need to be changed to being conditione if we have passed in the leaf node in the input_query
 	// TODO these might also not be a single return, assume single for the moment
-	q.CurrentLeaf = root.NextNode(nil)[0].NextNode(nil)[0]
+	q.FollowLeaf = root.NextNode(nil)[0].NextNode(nil)[0]
 
 	// TODO this need to be changed to being conditione if we have passed in the next node in the input_query
-	q.NextNode = root.NextNode(nil)[0].Value
-	// find the start node
-
-	// TODO test if nextNode is -1 and return error
+	// TODO these might also not be a single return, assume single for the moment
+	// TODO throw error if it cant find first node, spent way to long wondering why i got nil pointer deref
+	for _, edge := range data[root.NextNode(nil)[0].Value] {
+		if edge.EdgeName == q.FollowLeaf.Value {
+			q.NextNode = edge.TargetName
+			break
+		}
+	}
 
 	return q, nil
 }
 
 // This function converts the queryStruct to an string which could be passed on to another server
 func (q *QueryStruct) ToString() string {
-	return fmt.Sprintf("%s\n%s\n%d", q.Query, q.NextNode, q.CurrentLeaf.ID)
+	return fmt.Sprintf("%s\n%s\n%d", q.Query, q.NextNode, q.FollowLeaf.ID)
 }
 
 func (q *QueryStruct) DebugToString() string {
-	return fmt.Sprintf("%s\nNextNode: %s\nFollowingEdge: %d (%s)", q.Query, q.NextNode, q.CurrentLeaf.ID, q.CurrentLeaf.Value)
+	return fmt.Sprintf("%s\nNextNode: %s\nFollowingEdge: %d (%s)", q.Query, q.NextNode, q.FollowLeaf.ID, q.FollowLeaf.Value)
+}
+
+func (q *QueryStruct) next(data map[string][]DataEdge) []QueryStruct {
+	nextQ := make([]QueryStruct, 0)
+
+	// for each edge we want to follow
+	for _, follow_edge := range q.FollowLeaf.NextNode(nil) {
+
+		// for each edge that exist from node
+		for _, exist_edge := range data[q.NextNode] {
+
+			// if it exist and we want to follow it
+			if follow_edge.Value == exist_edge.EdgeName {
+				// create a new query with new current leaf
+				// and next node
+				copy := QueryStruct{
+					Query:       q.Query,
+					Rootpointer: q.Rootpointer,
+					FollowLeaf:  follow_edge,
+					NextNode:    exist_edge.TargetName,
+				}
+				nextQ = append(nextQ, copy)
+			}
+		}
+	}
+	return nextQ
 }
 
 func TestBob() {
-	data := map[int]DataNode{
-		1: {
-			"start",
-			[]DataEdge{{"from_s", 2}}},
-		2: {
-			"end",
-			[]DataEdge{},
+	data := map[string][]DataEdge{
+		"s": {
+			{"pickaxe", "pickaxe"},
+		},
+		"pickaxe": {
+			{"obtainedBy", "Pickaxe_From_Stick_And_Stone_Recipe"},
+		},
+		"Pickaxe_From_Stick_And_Stone_Recipe": {
+			{"hasInput", "Stick"},
+			{"hasInput", "Cobblestone"},
 		},
 	}
+	fmt.Printf("%v\n\n", data)
 
-	q, _ := bobTheBuilder("start/from_s", data)
+	q, _ := bobTheBuilder("s/pickaxe/{obtainedBy/hasInput}*", data)
+	fmt.Println(q.DebugToString())
+	fmt.Println()
 
-	fmt.Print(q.DebugToString())
+	q = q.next(data)[0]
+	fmt.Println(q.DebugToString())
+	fmt.Println()
+
+	q = q.next(data)[0]
+	fmt.Println(q.DebugToString())
+	fmt.Println()
+
 }
